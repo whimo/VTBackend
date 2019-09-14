e49cf731555130a5f70d4609ec507b545cc22c23
whimo <me@whimo.me>
2019-09-14 14:35:04 +0300

    Basic app structure

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app)
+
+lm = LoginManager(app)
+bcrypt = Bcrypt(app)
+
+from app import views, models
---
+++ config.py
@@ -0,0 +1,11 @@
+host = 'localhost'
+port = 5000
+
+from os import environ
+debug = True
+if environ.get('LASALCA_PROD'):
+    debug = False
+
+SQLALCHEMY_DATABASE_URI = 'postgres://vtb:vtb@vtb.whimo.me/vtb'
+if environ.get('LASALCA_SASALCA'):
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
