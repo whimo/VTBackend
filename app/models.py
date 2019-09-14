21ffbb020c7784fc3bfc2626c5119cb08dd0c145
whimo <me@whimo.me>
2019-09-14 17:45:00 +0300

    Relationship between users and discussions

--- app/models.py
+++ app/models.py
@@ -1,6 +1,14 @@
 from app import db
 
 
+discussion_members = db.Table('discussion_members',
+                              db.Column('discussion_id', db.Integer,
+                                        db.ForeignKey('discussion.id'), nullable=False),
+                              db.Column('user_id', db.Integer,
+                                        db.ForeignKey('user.id'), nullable=False),
+                              db.PrimaryKeyConstraint('discussion_id', 'user_id'))
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
@@ -36,3 +44,5 @@ class Discussion(db.Model):
 
     description = db.Column(db.Text, nullable=False)
     deadline =    db.Column(db.DateTime)
+
+    members = db.relationship('User', backref='discussions', secondary=discussion_members)
