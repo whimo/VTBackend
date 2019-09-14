b289cc0b545119ca633299d82c1d4df3430c12a8
whimo <me@whimo.me>
2019-09-14 21:27:38 +0300

    Login manager stuff

--- app/views.py
+++ app/views.py
@@ -1,8 +1,25 @@
-from app import app, models, db, bcrypt
-
-from flask import Flask
+from app import app, models, db, lm, bcrypt
+from flask import g
+from flask_login import current_user
 from graphene import ObjectType, String, Schema
 from flask_graphql import GraphQLView
+
+
+@app.before_request
+def before_request():
+    '''
+    Set current request user before every request
+    '''
+    g.user = current_user
+
+
+@lm.user_loader
+def load_user(userid):
+    '''
+    Flask-Login user loader
+    '''
+    return models.User.query.get(int(userid))
+
 
 class Query(ObjectType):
     hello = String(name=String(default_value="stranger"))
@@ -10,7 +27,7 @@ class Query(ObjectType):
 
     def resolve_vote(root, info, name, num_of_vote):
         return
-    
+
 
 schema = Schema(query=Query)
 
