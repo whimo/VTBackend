8fa8b62030d009556749c770c434739180c8de8b
whimo <me@whimo.me>
2019-09-14 15:23:08 +0300

    User model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,31 @@
 from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    email =    db.Column(db.String(60), nullable=False, index=True, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+
+    name =      db.Column(db.Text)
+    last_name = db.Column(db.Text)
+
+    vote_weight = db.Column(db.SmallInteger, default=1)
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
