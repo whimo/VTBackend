e7adbf3d643dcc0f67c0f7fa2dc9c3b308b5f8ce
whimo <me@whimo.me>
2019-09-15 02:04:52 +0300

    Graphene objects and query class

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from app import app, models, db, lm, bcrypt
 from flask import g
 from flask_login import current_user
 from graphene import ObjectType, String, Schema
+import graphene
+from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 from flask_graphql import GraphQLView
 
 
@@ -21,20 +23,61 @@ def load_user(userid):
     return models.User.query.get(int(userid))
 
 
+class User(SQLAlchemyObjectType):
+    class Meta:
+        model = models.User
+
+
+class Discussion(SQLAlchemyObjectType):
+    class Meta:
+        model = models.Discussion
+
+
+class Section(SQLAlchemyObjectType):
+    class Meta:
+        model = models.Section
+
+
+class Vote(SQLAlchemyObjectType):
+    class Meta:
+        model = models.Vote
+
+
+class Message(SQLAlchemyObjectType):
+    class Meta:
+        model = models.Message
+
 
 class Query(ObjectType):
-    vote = String(user_id=String(), section_id=String())
-    goodbye = String()
+    users =       graphene.List(User)
+    discussions = graphene.List(Discussion)
+    sections =    graphene.List(Section)
+    votes =       graphene.List(Vote)
+    messages =    graphene.List(Message)
 
-    def resolve_vote(root, info, user_id, section_id):
-        new_vote = models.Vote(user_id=user_id, section_id=section_id)
-        db.session.add(new_vote)
-        db.session.commit()
-        return True
+    def resolve_users(self, info):
+        query = User.get_query(info)
+        return query.all()
+
+    def resolve_discussions(self, info):
+        query = Discussion.get_query(info)
+        return query.all()
+
+    def resolve_sections(self, info):
+        query = Section.get_query(info)
+        return query.all()
+
+    def resolve_votes(self, info):
+        query = Vote.get_query(info)
+        return query.all()
+
+    def resolve_messages(self, info):
+        query = Message.get_query(info)
+        return query.all()
 
 
 schema = Schema(query=Query, auto_camelcase=False)
 
-view_func = GraphQLView.as_view("graphql", schema=schema)
+view_func = GraphQLView.as_view("graphql", schema=schema, graphiql=True)
 
 app.add_url_rule("/api", view_func=view_func)
