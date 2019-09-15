3cb866ac315c8e6f2c5d44dff89c1f1aae679d94
tanyaionova <isaqtm@gmail.com>
2019-09-15 04:10:33 +0300

    [test on prod] cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('config')
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
